2c1691e36c020adf92b81f06f38171707e6c7b41
Dipanshi Verma <dipanshiverma0@gmail.com>
2025-08-15 16:48:28 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
 function App() {
  
 
   return (
     <>
       <div>
-        <h1>Back Trackers </h1>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </>
   )
